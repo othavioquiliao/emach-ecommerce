c5019105118c5e846db9c9640b0dc677551ac488
Othavio Quiliao <othavioaqa@gmail.com>
2026-04-07 13:57:37 -0300

    refactor: remover dark mode toggle, adotar chiaroscuro por seções

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -2,18 +2,11 @@
 
 import { Toaster } from "@emach/ui/components/sonner";
 
-import { ThemeProvider } from "./theme-provider";
-
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="system"
-			disableTransitionOnChange
-			enableSystem
-		>
+		<>
 			{children}
 			<Toaster richColors />
-		</ThemeProvider>
+		</>
 	);
 }
