cb58eca643ccd145b82b59a57b2daffb2d839b2f
Larissa Alves <alves04.larissa@gmail.com>
2026-04-23 13:32:59 -0300

    feat: update product tabs layout and enhance related products display

--- apps/web/src/app/product/[slug]/_components/related-products.tsx
+++ apps/web/src/app/product/[slug]/_components/related-products.tsx
@@ -14,12 +14,12 @@ export function RelatedProducts({ products }: RelatedProductsProps) {
 
 	return (
 		<>
-			<Separator className="mt-[60px]" />
+			<Separator className="" />
 			<section className="px-20 pt-16 pb-20">
 				<h2 className="mb-6 font-display font-medium text-[28px]">
 					Você também pode gostar
 				</h2>
-				<div className="grid grid-cols-4 gap-6">
+				<div className="grid grid-cols-5 gap-6">
 					{products.map((product) => (
 						<ProductCard key={product.id} product={product} />
 					))}
